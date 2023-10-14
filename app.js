07320e65584d1370ea3f42d48a6b33fef8a9e347
Khushi Patel <khushipatel0147@gmail.com>
2023-10-14 16:49:27 +0530

    commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRoutes = require('./routes/user.routes');
+const todoRoutes = require('./routes/todo.routes');
 const body_parser = require('body-parser')
 
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 app.use(body_parser.json());
 
 app.use('/',userRoutes);
+app.use('/',todoRoutes);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const app = require("./app");
 const db = require('./config/db');
 const UserModel = require('./model/user.model');
+const TodoModel = require('./model/todo.model');
 
 const port = 3000;
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
+
 app.listen(port,()=>{
     console.log(`Server Listening on Port http://localhost:${port}`);
 });
